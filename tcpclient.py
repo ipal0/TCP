a61475ffc8a0053db3340e656b0383d4dab847b4
Pal <tecpal@users.noreply.github.com>
2015-03-05 11:54:20 -0500

    Update tcpclient.py

--- tcpclient.py
+++ tcpclient.py
@@ -1,6 +1,7 @@
+#TCP Client for Python3
 import socket
 from sys import argv
-import thread
+import _thread
 HOST,PORT = argv[1],int(argv[2]) 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
@@ -9,14 +10,14 @@ def Read(s):
   while Flag:
     try:
       rdata = s.recv(1024)
-      print "<<< %s" %rdata.strip()
+      print("Recd: %s" %rdata.strip().decode('utf'))
     except Exception:
-      thread.exit()
-thread.start_new_thread(Read,(s,))
+      _thread.exit()
+_thread.start_new_thread(Read,(s,))
 while Flag:
-  wdata = raw_input()
-  s.send(bytes(wdata+'\r'))
+  wdata = input()
   if wdata.strip() == 'quit':
-    print "connection closed"
+    print("connection closed")
     Flag = False
+  s.send(bytes(wdata,'utf'))
 s.close()
