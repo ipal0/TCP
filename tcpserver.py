ea75a14a86be557cfbb4ee5f17ea2c7d5566fbfb
Pal <tecpal@users.noreply.github.com>
2015-03-05 11:53:23 -0500

    Update tcpserver.py

--- tcpserver.py
+++ tcpserver.py
@@ -1,31 +1,33 @@
-# TCP Server Program
-import socket,thread
+# TCP Server Program for Python3
+import socket,_thread
 from time import ctime,sleep
 from sys import argv
-HOST_TCP = ''               # Symbolic name meaning all available interfaces
-PORT_TCP = int(argv[1])
+HOST = ''               # Symbolic name meaning all available interfaces
+PORT = int(argv[1])
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST_TCP, PORT_TCP))
+s.bind((HOST, PORT))
 s.listen(1)
-#s.settimeout(5)
 FLAG = 0
 def S_TCP(conn,addr):
  while(FLAG):
-  conn.send(ctime()+'\n')
+  conn.send(bytes(input(),'utf'))
   sleep(1)
 def TCP(conn,addr):
  global FLAG
  while 1:
-  DATA = conn.recv(1024).strip()
-  if DATA == 'send': 
-   FLAG = 1
-   thread.start_new_thread(S_TCP,(conn,addr))
-  elif DATA == 'stop': FLAG = 0
-  if DATA.strip() != "quit": conn.send(DATA.upper()+'\n')
-  else:
-   print addr[0], "connection closed" 
-   break
+  try:
+   DATA = conn.recv(1024).strip().decode('utf')
+   if DATA.strip() != "quit": 
+    FLAG = 1
+    print("Recd: %s" %DATA)
+    _thread.start_new_thread(S_TCP,(conn,addr))
+   else:
+    FLAG = 0
+    print(addr[0], "connection closed")
+    break
+  except Exception:
+    _thread.exit()
 while 1:
   conn, addr = s.accept()
   print('Connected by', addr[0])
-  thread.start_new_thread(TCP,(conn,addr))
+  _thread.start_new_thread(TCP,(conn,addr))
